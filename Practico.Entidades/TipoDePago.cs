9651335dda4ea72008e642c790274bdc6eaf6bbc
tomas porta <portatomas55@outlook.com>
2025-05-22 14:25:00 -0300

    Agregar archivos de proyecto.

---
+++ Practico.Entidades/TipoDePago.cs
@@ -0,0 +1,8 @@
+﻿namespace Practico.Entidades
+{
+    public class FormaDePago
+    {
+        public int FormaDePagoId { get; set; }
+        public string Descripcion { get; set; } = null!;
+    }
+}
---
+++ PracticoDeSeminario2025/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticoDeSeminario2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
